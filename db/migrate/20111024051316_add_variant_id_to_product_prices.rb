a155d8d727c6d24059e02f26c713c498aef6337d
elvira lacap <matet.lacap@gmail.com>
2011-10-24 18:00:21 +0800

    fix migration2

--- db/migrate/20111024051316_add_variant_id_to_product_prices.rb
+++ db/migrate/20111024051316_add_variant_id_to_product_prices.rb
@@ -1,7 +1,7 @@
 class AddVariantIdToProductPrices < ActiveRecord::Migration
   def change
     change_table :product_prices do |t|
-      t.integer  "variant_id",                                    :null => false
+      t.integer  "variant_id"
     end
   end
 end
